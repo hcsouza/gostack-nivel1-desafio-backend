a0135444f1f11998b5cddcea83b9d77e1649ef4e
Hugo Souza <hugo.souza@ifood.com.br>
2020-04-11 19:00:15 -0300

    Implements all routes to deal with repositories

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
-
+const { uuid } = require("uuidv4");
 const app = express();
 
 app.use(express.json());
@@ -11,23 +9,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { 
+      id: uuid(),
+      title, 
+      url,
+      techs,
+      likes: 0,
+  }
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", repositoryExistsMiddleware, (request, response) => {
+
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const foundIndex  = getRepositoryIndex(id);
+
+  const updatedRepository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[foundIndex].likes,
+  }
+
+  repositories[foundIndex] = updatedRepository;
+  return response.json(updatedRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", repositoryExistsMiddleware, (request, response) => {
+  const { id } = request.params;
+  const foundIndex = getRepositoryIndex(id);
+
+  repositories.splice(foundIndex, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", repositoryExistsMiddleware, (request, response) => {
+  const { id } = request.params;
+  const foundIndex = getRepositoryIndex(id);
+
+  repositories[foundIndex].likes++; 
+  return response.json(repositories[foundIndex]);
 });
+
+function getRepositoryIndex(id) {
+  return repositories.findIndex( searchedRepository => 
+    searchedRepository.id === id
+  );
+}
+
+function repositoryExistsMiddleware(request, response, next) {
+  const foundIndex = getRepositoryIndex(request.params.id); 
+
+  if(foundIndex < 0) {
+    return response.status(400).json({error: "Repository not found!"});
+  }
+  next();
+}
 
 module.exports = app;
